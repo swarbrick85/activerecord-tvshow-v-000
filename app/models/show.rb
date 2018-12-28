3e06fae1ceaefd8baf4b3a58117af4583be6e7e6
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-28 18:19:37 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,14 @@
+class Show < ActiveRecord::Base 
+  
+  def self.highest_rating 
+    ((self.all.sort_by { |show| show.rating}).reverse[0]).rating
+  end 
+  
+  def self.most_popular_show 
+    ((self.all.sort_by { |show| show.rating}).reverse[0])
+  end 
+  
+  def self.lowest_rating
+    ((self.all.sort_by { |show| show.rating})[0]).rating
+  end 
+end 
